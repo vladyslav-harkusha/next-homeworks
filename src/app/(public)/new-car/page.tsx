e23d9510d1cf5aed086c08d1551412db380b9ca3
Vlad Harkusha <sivari888@gmail.com>
2025-02-10 14:33:20 +0200

    first commit

--- src/app/(public)/new-car/page.tsx
+++ src/app/(public)/new-car/page.tsx
@@ -1,22 +1,8 @@
 import {FC} from "react";
 import NewCarForm from "@/components/new-car-form/NewCarForm";
 import './new-car-page.scss';
-import {SearchParams} from "next/dist/server/request/search-params";
-import {postCar} from "@/services/cars.api.service";
-import {INewCarData} from "@/models/INewCarData";
 
-type Props = {
-    searchParams: Promise<SearchParams>;
-}
-
-const NewCarPage: FC<Props> = async ({ searchParams }) => {
-    const awaitedSP = await searchParams;
-
-
-    // if (awaitedSP) {
-    //     await postCar({ brand: awaitedSP.brand, price: awaitedSP.price, year: awaitedSP.year });
-    // }
-
+const NewCarPage: FC = async () => {
 
     return (
         <div className='new-car-page'>
